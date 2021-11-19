1e390027798d32f1fd59c42d9849d801c34e6782
Ethan Cemer <tylercemer@gmail.com>
2021-11-19 16:08:04 -0600

    Completed Swap smart contract and test

--- src/contracts/Swap.sol
+++ src/contracts/Swap.sol
@@ -12,55 +12,64 @@ interface IUniSwapRouter is ISwapRouter {
 contract Swap {
     uint24 public constant poolFee = 3000;
     address public dai;
-    address public weth9;
+    address public weth;
     IUniSwapRouter public constant swapRouter = IUniSwapRouter(0xE592427A0AEce92De3Edee1F18E0157C05861564);
     IQuoter public constant quoter = IQuoter(0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6);
     event Converted (
-        address _user,
-        uint256 _amountOut
+        address user,
+        uint256 amountOut,
+        uint256 amountInMaximum,
+        uint256 amountIn
     );
-    constructor(address _dai, address _weth9) {
+    constructor(address _dai, address _weth) {
         dai = _dai;
-        weth9 = _weth9;
+        weth = _weth;
     }
 
     // Used to accept swapRouter refund 
     receive() external payable {
     }
     // This function is a read only function but can't be marked as view because it relies on non-view functiions to compute the result.
-    function getEthInputAmount(uint _amountOut) external payable returns(uint256 _amountIn) {
-        require(_amountOut > 0, "Error, DAI amount out must be greater than 0");
-        address _tokenIn = weth9;
+    function getEthInputAmount(uint256 _amountOut) external payable returns(uint256 _amountIn) {
+        address _tokenIn = weth;
         address _tokenOut = dai;
-        uint24 fee = 500;
+        uint24 _fee = 500;
         uint160 sqrtPriceLimitX96 = 0;
         _amountIn = quoter.quoteExactOutputSingle(
             _tokenIn,
             _tokenOut,
-            fee,
+            _fee,
             _amountOut,
             sqrtPriceLimitX96
         );
     }
 
-    function convertEthToExactDai(uint256 _amountOut) external payable {
+    function convertEthToExactDai(uint256 _daiAmountOut, uint256 _deadline) external payable {
+        require(_daiAmountOut > 0, "Error, DAI amount out must be greater than 0");
+        require(msg.value > 0, "Error, ETH amount must be greater than 0");
+        address _tokenIn = weth;
+        address _tokenOut = dai;
+        uint24 _fee = poolFee;
+        address _recipient = msg.sender;
+        uint256 _amountInMaximum = msg.value;
+        uint160 _sqrtPriceLimitX96 = 0;
         ISwapRouter.ExactOutputSingleParams memory params =
-            ISwapRouter.ExactOutputSingleParams({
-                tokenIn: weth9,
-                tokenOut: dai,
-                fee: poolFee,
-                recipient: msg.sender,
-                deadline: block.timestamp,
-                amountOut: _amountOut,
-                amountInMaximum: msg.value,
-                sqrtPriceLimitX96: 0
-            });
+            ISwapRouter.ExactOutputSingleParams(
+                _tokenIn,
+                _tokenOut,
+                _fee,
+                _recipient,
+                _deadline,
+                _daiAmountOut,
+                _amountInMaximum,
+                _sqrtPriceLimitX96
+            );
 
-        swapRouter.exactOutputSingle(params);
+        uint256 _amountIn = swapRouter.exactOutputSingle{value: msg.value}(params);
         swapRouter.refundETH();
         // Send the refunded ETH back to sender
         (bool success, ) = msg.sender.call{value: address(this).balance }('');
         require(success, "Transfer failed");
-        emit Converted (msg.sender, _amountOut);
+        emit Converted (msg.sender, _daiAmountOut, msg.value, _amountIn);
     }
 }
--- test/Swap.test.js
+++ test/Swap.test.js
@@ -1,6 +1,7 @@
-
 const Web3 = require('web3') 	
 const web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:8545'))
+import { expect } from 'chai'
+import { futureTime } from './helpers'
 import { contractsLoadedSelector } from '../src/store/selectors'
 const { expectRevert, time } = require('@openzeppelin/test-helpers')
 const Swap = artifacts.require("Swap")
@@ -12,25 +13,45 @@ require('chai')
 	.use(require('chai-as-promised'))
 	.should()
 
-contract("Swap",(accounts) => {
+contract("Swap",([deployer, user1]) => {
     const poolFee = "3000"
     const daiAddress = "0x6B175474E89094C44Da98b954EedeAC495271d0F"
-    const weth9Address = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
+    const wethAddress = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
     const swapRouter = "0xE592427A0AEce92De3Edee1F18E0157C05861564"
     const quoter = "0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6"
-    let swap
+    let swap, swapRef
+    // spliced ERC20 token
+    let splicedAbi = [
+        // balanceOf
+        {
+          "constant":true,
+          "inputs":[{"name":"_owner","type":"address"}],
+          "name":"balanceOf",
+          "outputs":[{"name":"balance","type":"uint256"}],
+          "type":"function"
+        },
+        // decimals
+        {
+          "constant":true,
+          "inputs":[],
+          "name":"decimals",
+          "outputs":[{"name":"","type":"uint8"}],
+          "type":"function"
+        }
+      ];
     beforeEach(async() => {
-        swap = await Swap.new(daiAddress, weth9Address)
+        swap = await Swap.new(daiAddress, wethAddress)
+        swapRef = new web3.eth.Contract(Swap.abi, swap.address)
     })
     describe("deployment", () => {
         let result
-        it("tracks the poolFee, dai address and weth9 address", async() => {
+        it("tracks the poolFee, dai address and weth address", async() => {
             result = await swap.poolFee();
             result.toString().should.equal(poolFee);
             result = await swap.dai();
             result.toString().should.equal(daiAddress)
-            result = await swap.weth9();
-            result.toString().should.equal(weth9Address)
+            result = await swap.weth();
+            result.toString().should.equal(wethAddress)
         })
         it("tracks the uniswap swapRouter and quoter contract addresses", async() => {
             result = await swap.swapRouter();
@@ -42,17 +63,75 @@ contract("Swap",(accounts) => {
     describe("get ETH input amount for a given DAI output", () => {
         let result
         let daiAmount = toWei(50)
-        it("rejects when DAI amount out is zero", () => {
-            expectRevert(swap.getEthInputAmount('0'), 'Error, DAI amount out must be greater than 0')
-        })
         it("returns ETH input value", async() => {
-            let swapRef = new web3.eth.Contract(Swap.abi, swap.address)
+            // Must be called off chain using web3 .call() method
             result = await swapRef.methods.getEthInputAmount(daiAmount).call()
             result.toString().length.should.be.at.least(1, 'did not return ETH input value ')
             console.log(`Exact DAI output: ${fromWei(daiAmount)}`)
             console.log(`Returned ETH input: ${fromWei(result)}`)
-            console.log(`DAI/ETH ratio: ${result/DAI}`)
-            console.log(`compared price DAI/ETH at e.g https://info.uniswap.org/#/pools/0xc2e9f25be6257c210d7adf0d4cd6e3e881ba25f8 e.g 0.00035312`)
+            console.log(`DAI/ETH ratio: ${result/daiAmount}`)
+            console.log(`Compare price DAI/ETH at e.g https://info.uniswap.org/#/pools/0xc2e9f25be6257c210d7adf0d4cd6e3e881ba25f8 e.g 0.00031412`)
+        })
+    })
+    describe("converting ETH to DAI", () => {
+        describe("success", () => {
+            let ethBalance
+            let daiBalance
+            let daiRef
+            let quotedEthAmountIn
+            let maxEthAmountIn
+            let ethAmountIn
+            let daiAmountOut = toWei(100) // 100 dai out
+            let result
+            beforeEach(async() => {
+                // load dai contract
+                daiRef = new web3.eth.Contract(splicedAbi, daiAddress)
+    
+                // start Ether and DAI balance before swap
+                ethBalance = await web3.eth.getBalance(user1) //BN
+                console.log(`start ETH balance: ${fromWei(ethBalance)}`)
+    
+                daiBalance = await daiRef.methods.balanceOf(user1).call()
+                console.log(`start DAI balance: ${fromWei(ethBalance)}`)
+                //Quote about how much Eth is needed to get an exact amount of Dai
+                quotedEthAmountIn = await swapRef.methods.getEthInputAmount(daiAmountOut).call()
+                console.log(`The quoted ETH amount in for an exact DAI output of ${fromWei(daiAmountOut)} is ${fromWei(quotedEthAmountIn)}`)
+                // Use that quoted amount to determine the max amount of ether to send with the convertEthToExactDai function
+                // Will send 110% of the quoted amount to ensure the transaction goes through 
+                // The ether thats left over after the swap has been executed will be sent back to the user
+                maxEthAmountIn = 1.1*(quotedEthAmountIn)
+                console.log(`Max ETH amount input is ${maxEthAmountIn}, which is equal to 110% of the quoted ETH amout in`)
+                // user1 approves swap contract to spend their Dai
+                result = await swap.convertEthToExactDai(daiAmountOut, futureTime(500), {from : user1, value: maxEthAmountIn})
+            })
+            it("emits a Converted event", async() => {
+                const log = result.logs[0]
+                log.event.should.eq('Converted')
+                const event = log.args
+                event.user.should.eq(user1, "user is incorrect")
+                event.amountOut.toString().should.eq(daiAmountOut.toString(), "amount out is incorrect")
+                event.amountInMaximum.toString().should.eq(maxEthAmountIn.toString(), "max amount in is incorrect")
+                // Amount in should be less than or equal to max amount in
+                ethAmountIn = event.amountIn
+                expect(+ethAmountIn.toString()).to.be.at.most(+maxEthAmountIn.toString(), "amount in is incorrect")
+            })
+            it("successfully executes swap and refunds excess ether back to user", async() => {
+                const newEthBalance = await web3.eth.getBalance(user1) //Actual Eth balance after swap function executed
+                const subtractedEthBalance = +ethBalance.toString() - +ethAmountIn.toString() //Subtracted ETH balance excluding fees
+                // There should be no ether left in the smart contract. All of it should have been refunded
+                const swapContractBalance = await web3.eth.getBalance(swap.address)
+                swapContractBalance.toString().should.eq('0')
+                // The ether paid upfront, maxEthAmount, should be less than or equal to the ethAmountIn
+                expect(+maxEthAmountIn.toString()).to.be.at.least(+ethAmountIn.toString())
+                const percentError = 100*Math.abs(ethAmountIn - quotedEthAmountIn)/(ethAmountIn)
+                console.log(`The percent error between the quoted ETH amount in and the actual ETH amount in is ${percentError}%`)
+                // newEthBalance should be less than subtracted Eth balance due to fees
+                expect(+newEthBalance.toString()).to.be.below(+subtractedEthBalance.toString())
+                console.log(`ETH balance after swap: ${fromWei(newEthBalance)} should be approximately ${fromWei(subtractedEthBalance)}`)
+                const newDaiBalance = await daiRef.methods.balanceOf(user1).call()
+                newDaiBalance.toString().should.eq((+daiBalance.toString()+ +daiAmountOut.toString()).toString())
+                console.log(`DAI balance after swap: ${fromWei(newDaiBalance)}`)
+            })
         })
     })
 })
