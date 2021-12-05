14f393433fbf6d247db09fbc2f7179575818c38c
Ethan Cemer <tylercemer@gmail.com>
2021-12-05 16:04:31 -0600

    Updated test hlpers file and removed unnecessary react files

--- test/helpers.js
+++ test/helpers.js
@@ -15,5 +15,5 @@ export const wait = (seconds) => {
 }
 export const mainnetDai = "0x6B175474E89094C44Da98b954EedeAC495271d0F"
 export const mainnetWeth = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
-export const ropstenDai = "0x57aC66399420f7C99F546A5a7C00e0D0Ff2679E1"
+export const ropstenDai = "0xad6d458402f60fd3bd25163575031acdce07538d"
 export const ropstenWeth = "0xc778417e063141139fce010982780140aa0cd5ab"
